1809b592e708cf7742c6767c531c4196fb4fd7c5
petter07080900 <56706484+petter07080900@users.noreply.github.com>
2019-11-08 12:10:34 +0800

    設定腳本

---
+++ UnityProject/Assets/語言/PLAYER.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class PLAYER : MonoBehaviour
+{
+    [Header("速度"),Range(0f, 100f)]
+    public float speed = 3.5f; //浮點數 - 結尾要有F
+    [Header("跳躍"),Range(10,100)]
+    public int jump = 300;                 // 整數
+    [Header("是否在地板上"),Tooltip("用來判定是否在地板上。")]
+    public bool isGround = false;                //布林值  true . false
+   [Header("姓名")]
+    public string name = "slagmale";              //字串 - 需要用雙引號
+}
