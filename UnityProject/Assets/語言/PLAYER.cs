b29ad9d2a1caf073f053fe6b16ac0cb3fefb00a4
petter07080900 <56706484+petter07080900@users.noreply.github.com>
2019-12-06 11:05:24 +0800

    20191206

--- UnityProject/Assets/語言/PLAYER.cs
+++ UnityProject/Assets/語言/PLAYER.cs
@@ -1,5 +1,6 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PLAYER : MonoBehaviour
 {
@@ -18,31 +19,35 @@ public class PLAYER : MonoBehaviour
     [Header("音效區域")]
     public AudioSource aud;
     public AudioClip soundDiamond;
+    [Header("鑽石區域")]
+    public int diamondCurrent;
+    public int diamondTotal;
+    public Text textDiamond;
     #endregion
 
-     //定義方法
-     //語法
-     //修飾詞 傳回類型 方法名稱(){}
-     //void 無傳回 
+    //定義方法
+    //語法
+    //修飾詞 傳回類型 方法名稱(){}
+    //void 無傳回 
 
-     private void Move()
+    private void Move()
     {
-        float h =Input.GetAxisRaw("Horizontal");  // 輸入.取得軸向("水平")左右與AD
-        r2d.AddForce(new Vector2( speed * h, 0));
+        float h = Input.GetAxisRaw("Horizontal");  // 輸入.取得軸向("水平")左右與AD
+        r2d.AddForce(new Vector2(speed * h, 0));
         ani.SetBool("跑步開關", h != 0);        //動畫元件.設定布林值
 
         //如果按下A或者左右鍵 角度=(0，180，0)
 
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) transform.eulerAngles = new Vector3(0, 180, 0);
-       else  if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) transform.eulerAngles = new Vector3(0, 0, 0);
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) transform.eulerAngles = new Vector3(0, 0, 0);
 
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space)&& isGround==true)
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
         {
-           
+
             isGround = false;   //在地板上=取消
             r2d.AddForce(new Vector2(0, jump)); //鋼體.推力(往上)
             ani.SetTrigger("跳躍觸發");    //動畫元件3設定觸發器("參數")
@@ -55,11 +60,18 @@ public class PLAYER : MonoBehaviour
 
     //事件: 在特定的時間點已指定次數執行
     //更新事件:秒執行約60次 (60FRS)
+    private void Start()
+    {
+        //鑽石總數=尋找所有指定標籤物件("指定標籤").數量
+        diamondTotal=GameObject.FindGameObjectsWithTag("鑽石").Length;
+        //更新介面
+        textDiamond.text="鑽石:0/"+diamondTotal;
+    }
     private void Update()
     {
         Move();
         Jump();
-    }    
+    }
 
     //碰撞事件:2D 物件碰撞時執行一次
     //collision 嵾數 : 碰到物件的資訊
@@ -68,6 +80,18 @@ public class PLAYER : MonoBehaviour
         if (collision.gameObject.name == "地板")
         {
             isGround = true;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "鑽石")
+        {  
+            aud.PlayOneShot(soundDiamond, 1.5f);
+            Destroy(collision.gameObject);
+            diamondCurrent ++;
+            textDiamond.text = "鑽石:" +diamondCurrent + "/" + diamondTotal;
         }
     }
 }
